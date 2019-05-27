5ccbf65096e6ff64362f51e014049f2d1b4c12a9
J-Agens <johntagens@gmail.com>
2019-05-27 19:29:36 -0400

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user
+  # before_action :authenticate_user
   # Needs to be placed in before action for certain controllers
   def authenticate_user
     if !current_user
